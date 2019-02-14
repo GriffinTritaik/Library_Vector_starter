a7408052181e9297587f01bf826d54810d5bfed5
guest-lxthsc <guest-lxthsc@L221-0000>
2019-02-14 17:20:13 -0500

    Got IO part 1 done

--- library/fileIO.cpp
+++ library/fileIO.cpp
@@ -3,22 +3,12 @@
 #include <sstream>
 #include <string>
 #include "../includes_usr/fileIO.h"
+#include "../includes_usr/constants.h"
+#include "../includes_usr/datastructures.h"
 using namespace std;
 
-bool readFileIntoVector(ifstream &file) {
-	bool retValue = true;
 
-	std::string line;
-	std::string token;
 
-	return retValue;
-}
-
-void processLine(std::string &myString){
-    std::stringstream ss(myString);
-    std::string token;
-
-}
 /* clears, then loads books from the file filename
  * returns  COULD_NOT_OPEN_FILE if cannot open filename
  * 			NO_BOOKS_IN_LIBRARY if there are 0 entries in books
@@ -26,16 +16,40 @@ void processLine(std::string &myString){
  * */
 int loadBooks(std::vector<book> &books, const char* filename)
 {
+	books.clear();
+
 	ifstream myFile;
 	myFile.open(filename);
 	if (!myFile.is_open()){
 		return COULD_NOT_OPEN_FILE;
 	}
 
-	if (!readFileIntoVector(myFile)){
+	string line;
+	string token;
+
+	while (getline(myFile, line)){
+		book toAdd;
+
+		stringstream ss(line);
+		getline(ss, token, ',');
+		toAdd.book_id = std::stoi(token);
+		getline(ss, token, ',');
+		toAdd.title = token;
+		getline(ss, token, ',');
+		toAdd.author = token;
+		getline(ss, token, ',');
+		toAdd.state = IN;
+		getline(ss, token);
+		toAdd.loaned_to_patron_id = std::stoi(token);
+
+		books.push_back(toAdd);
+	}
+
+	if (books.empty()){
 		return NO_BOOKS_IN_LIBRARY;
 	}
 
+	myFile.close();
 	return SUCCESS;
 }
 
@@ -46,12 +60,23 @@ int loadBooks(std::vector<book> &books, const char* filename)
  * */
 int saveBooks(std::vector<book> &books, const char* filename)
 {
-	ifstream myFile;
+	ofstream myFile;
 	myFile.open(filename);
 	if (!myFile.is_open()){
 		return COULD_NOT_OPEN_FILE;
 	}
 
+	if (books.empty()){
+		return NO_BOOKS_IN_LIBRARY;
+	}
+
+	for (int i = 0; i < books.size(); i++){
+		myFile << books[i].book_id << "," << books[i].title << "," <<
+				books[i].author << "," << books[i].state << "," <<
+				books[i].loaned_to_patron_id << endl;
+	}
+
+	myFile.close();
 	return SUCCESS;
 }
 
@@ -62,12 +87,34 @@ int saveBooks(std::vector<book> &books, const char* filename)
  * */
 int loadPatrons(std::vector<patron> &patrons, const char* filename)
 {
+	patrons.clear();
+
 	ifstream myFile;
 	myFile.open(filename);
 	if (!myFile.is_open()){
 		return COULD_NOT_OPEN_FILE;
 	}
 
+	string line;
+	string token;
+
+	while (getline(myFile, line)){
+		patron toAdd;
+
+		stringstream ss(line);
+		getline(ss, token, ',');
+		toAdd.patron_id = std::stoi(token);
+		getline(ss, token, ',');
+		toAdd.name = token;
+		getline(ss, token);
+		toAdd.number_books_checked_out = std::stoi(token);
+
+		patrons.push_back(toAdd);
+	}
+
+	if (patrons.empty()){
+		return NO_BOOKS_IN_LIBRARY;
+	}
 
 	return SUCCESS;
 }
